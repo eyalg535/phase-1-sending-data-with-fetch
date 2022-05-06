5a64a18e8964e4faf3f0d59ad396d549742f9aff
eyal gerby <eyalg535@gmail.com>
2022-05-06 13:29:55 -0400

    commit tree 68b3aa2fc03a6e9cf110c3b6ae960c3d38d1a49c

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
-// Add your code here
+const body = document.getElementsByTagName('body')[0];
+ const message ="Unauthorized Access"
+ function submitData(firstName, email) {
+     const confgObj = {
+        method: 'POST',
+        headers: {
+            'Content-type': 'application/json',
+            'Accept': 'application/json'
+     },
+     body: JSON.stringify({
+         'name': firstName,
+         'email': email
+     })
+     }
+    return fetch('http://localhost:3000/users', confgObj)
+    .then(res => res.json())
+    .then(body => addNewContact(body.id))
+    
+    .catch(error => message )
+        
+}
+ 
+
+function addNewContact(newID) {
+    document.getElementsByTagName('body')[0].innerHTML = newID;
+    document.body.append(message)
+}
+submitData('name', 'name@name.com')
+
